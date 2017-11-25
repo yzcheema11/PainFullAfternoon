3a8cbf87f7679e57c3a201efd814fa3ed8020f56
zan <zan@zipcoders-MacBook-Pro.local>
2017-11-25 12:39:00 -0500

    need to finish matching 2

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -17,29 +17,36 @@ public class ItemParser {
 
         ArrayList<String> valuePairs = findKeyValuePairsInRawItemData(rawItem);
 
-        String itemName = valuePairs.get(0);
         Pattern itemNamePattern = Pattern.compile("([n|N][a|A][m|M][e|E]):([a-z|A-Z|0-9]+)");
+        Pattern itemPricePattern = Pattern.compile("([p|P][r|R][i|I][c|C][e|E]):([0-9]+\\.[0-9]+)");
+        Pattern itemTypePattern = Pattern.compile("([t|T][y|Y][p|P][e|E]):([a-z|A-z]+)");
+        Pattern itemExpPattern = Pattern.compile("([e|E][x|X][p|P][i|I][r|R][a|A][t|T][i|I][o|O|0][n|N]):([0-9]+\\/[0-9]+\\/[0-9]+)");
+
+
+        String itemName = valuePairs.get(0);
         itemName = compareMatch(itemNamePattern,itemName);
 
         String itemPrice = valuePairs.get(1);
-        Pattern itemPricePattern = Pattern.compile("([p|P][r|R][i|I][c|C][e|E]):([0-9]+\\.[0-9]+)");
         itemPrice = compareMatch(itemPricePattern,itemPrice);
 
         String itemType = valuePairs.get(2);
-        Pattern itemTypePattern = Pattern.compile("([t|T][y|Y][p|P][e|E]):([a-z|A-z]+)");
         itemType = compareMatch(itemTypePattern,itemType);
 
         String itemExp = valuePairs.get(3);
-        Pattern itemExpPattern = Pattern.compile("([e|E][x|X][p|P][i|I][r|R][a|A][t|T][i|I][o|O|0][n|N]):([0-9]+\\/[0-9]+\\/[0-9]+)");
         itemExp = compareMatch(itemExpPattern,itemExp);
 
         return new Item(itemName, Double.parseDouble(itemPrice), itemType, itemExp);
     }
 
-    private String compareMatch(Pattern pattern, String string) {
-        
+    private String compareMatch(Pattern pattern, String stringValue) throws ItemParseException {
+        if(pattern.matcher(stringValue).matches()){
+            ArrayList<String> splitValue = splitStringWithRegexPattern(":",stringValue);
+            stringValue = splitValue.get(1).toUpperCase();
+            if
 
-        return string;
+        }
+
+        return stringValue;
     }
 
     public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem){
