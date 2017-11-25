17b6b77de329cafa5a9310b937c3b4f00b46fa91
zan <zan@zipcoders-MacBook-Pro.local>
2017-11-25 12:17:04 -0500

    need to finish matching

--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -2,6 +2,7 @@ package io.zipcoder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.regex.*;
 
 public class ItemParser {
 
@@ -13,11 +14,36 @@ public class ItemParser {
     }
 
     public Item parseStringIntoItem(String rawItem) throws ItemParseException{
-        return null;
+
+        ArrayList<String> valuePairs = findKeyValuePairsInRawItemData(rawItem);
+
+        String itemName = valuePairs.get(0);
+        Pattern itemNamePattern = Pattern.compile("([n|N][a|A][m|M][e|E]):([a-z|A-Z|0-9]+)");
+        itemName = compareMatch(itemNamePattern,itemName);
+
+        String itemPrice = valuePairs.get(1);
+        Pattern itemPricePattern = Pattern.compile("([p|P][r|R][i|I][c|C][e|E]):([0-9]+\\.[0-9]+)");
+        itemPrice = compareMatch(itemPricePattern,itemPrice);
+
+        String itemType = valuePairs.get(2);
+        Pattern itemTypePattern = Pattern.compile("([t|T][y|Y][p|P][e|E]):([a-z|A-z]+)");
+        itemType = compareMatch(itemTypePattern,itemType);
+
+        String itemExp = valuePairs.get(3);
+        Pattern itemExpPattern = Pattern.compile("([e|E][x|X][p|P][i|I][r|R][a|A][t|T][i|I][o|O|0][n|N]):([0-9]+\\/[0-9]+\\/[0-9]+)");
+        itemExp = compareMatch(itemExpPattern,itemExp);
+
+        return new Item(itemName, Double.parseDouble(itemPrice), itemType, itemExp);
+    }
+
+    private String compareMatch(Pattern pattern, String string) {
+        
+
+        return string;
     }
 
     public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem){
-        String stringPattern = "[;|^]";
+        String stringPattern = "[;|^|%|@|*|!]";
         ArrayList<String> response = splitStringWithRegexPattern(stringPattern , rawItem);
         return response;
     }
