ca7641b7daa3c0e61871f5a90d26556b1ed143e8
zan <zan@zipcoders-MacBook-Pro.local>
2017-11-25 14:54:37 -0500

    reworking it all

--- src/main/java/io/zipcoder/ItemParseException.java
+++ src/main/java/io/zipcoder/ItemParseException.java
@@ -1,4 +1,7 @@
 package io.zipcoder;
 
 public class ItemParseException extends Exception {
+    public ItemParseException(String s) {
+        super(s);
+    }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -13,41 +13,58 @@ public class ItemParser {
         return response;
     }
 
-    public Item parseStringIntoItem(String rawItem) throws ItemParseException{
+    public Item parseStringIntoItem(String rawItem) throws ItemParseException {
 
-        ArrayList<String> valuePairs = findKeyValuePairsInRawItemData(rawItem);
+        //ArrayList<String> valuePairs = findKeyValuePairsInRawItemData(rawItem);
 
-        Pattern itemNamePattern = Pattern.compile("([n|N][a|A][m|M][e|E]):([a-z|A-Z|0-9]+)");
+        Pattern itemNamePattern = Pattern.compile("(^[n|N][a|A][m|M][e|E]):([a-z|A-Z|0-9]+)");
         Pattern itemPricePattern = Pattern.compile("([p|P][r|R][i|I][c|C][e|E]):([0-9]+\\.[0-9]+)");
         Pattern itemTypePattern = Pattern.compile("([t|T][y|Y][p|P][e|E]):([a-z|A-z]+)");
-        Pattern itemExpPattern = Pattern.compile("([e|E][x|X][p|P][i|I][r|R][a|A][t|T][i|I][o|O|0][n|N]):([0-9]+\\/[0-9]+\\/[0-9]+)");
+        Pattern itemExpPattern = Pattern.compile("([e|E][x|X][p|P][i|I][r|R][a|A][t|T][i|I][o|O|0][n|N]):([0-9]+\\\\/[0-9]+\\\\/[0-9]+)");
+        Pattern itemSplit = Pattern.compile(":");
 
+        Matcher matchItemName = itemNamePattern.matcher(rawItem);
+        Matcher matchItemPrice = itemPricePattern.matcher(rawItem);
+        Matcher matchItemType = itemTypePattern.matcher(rawItem);
+        Matcher matchItemExp = itemExpPattern.matcher(rawItem);
+        int count = 0;
 
-        String itemName = valuePairs.get(0);
-        itemName = compareMatch(itemNamePattern,itemName);
-
-        String itemPrice = valuePairs.get(1);
-        itemPrice = compareMatch(itemPricePattern,itemPrice);
-
-        String itemType = valuePairs.get(2);
-        itemType = compareMatch(itemTypePattern,itemType);
-
-        String itemExp = valuePairs.get(3);
-        itemExp = compareMatch(itemExpPattern,itemExp);
-
-        return new Item(itemName, Double.parseDouble(itemPrice), itemType, itemExp);
-    }
-
-    private String compareMatch(Pattern pattern, String stringValue) throws ItemParseException {
-        if(pattern.matcher(stringValue).matches()){
-            ArrayList<String> splitValue = splitStringWithRegexPattern(":",stringValue);
-            stringValue = splitValue.get(1).toUpperCase();
-            if
-
+        //        String itemName = itemNamePattern;
+//        itemName = compareMatch(itemNamePattern,itemName);
+//
+//        String itemPrice = valuePairs.get(1);
+//        itemPrice = compareMatch(itemPricePattern,itemPrice);
+//
+//        String itemType = valuePairs.get(2);
+//        itemType = compareMatch(itemTypePattern,itemType);
+//
+//        String itemExp = valuePairs.get(3);
+//        itemExp = compareMatch(itemExpPattern,itemExp);
+//
+//        return new Item(itemName, Double.parseDouble(itemPrice), itemType, itemExp);
+        while (matchItemName.find()) try {
+            String name = matchItemName.group(0);
+            String[] keyValue = itemSplit.split(name);
+            String nameValue = keyValue[1];
+            return new Item(name, Double.parseDouble(price), type, exp);
+        } catch (ItemParseException e) {
+            count++;
         }
-
-        return stringValue;
+        return  null;
     }
+
+//    private String compareMatch(Pattern pattern, String stringValue) throws ItemParseException {
+//        if(pattern.matcher(stringValue).matches()) {
+//            ArrayList<String> splitValue = splitStringWithRegexPattern(":", stringValue);
+//            stringValue = splitValue.get(1).toUpperCase();
+//        }
+//
+//        else {
+//            throw new ItemParseException("This item can't be parsed: " + stringValue);
+//        }
+//
+//        return stringValue;
+//    }
 
     public ArrayList<String> findKeyValuePairsInRawItemData(String rawItem){
         String stringPattern = "[;|^|%|@|*|!]";
